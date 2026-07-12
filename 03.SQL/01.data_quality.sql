5703f36983d2babb6d9738c88de1669310dfbad1
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-07-12 11:01:55 +0200

    Update 01.data_quality.sql

--- 03.SQL/01.data_quality.sql
+++ 03.SQL/01.data_quality.sql
@@ -209,4 +209,126 @@ go
 -----------------------------------------------------------------
 -----------------------------------------------------------------
 
+--------------------
+--3. Order_items
+--------------------
+go
+
+--show data 
+SELECT *
+FROM   order_items;
+
+go
+
+-- row count
+SELECT Count(*) AS total_rows
+FROM   order_items;
+
+go
+
+-- uniqueness 
+SELECT Count(DISTINCT order_item_id) AS u_order_item
+FROM   order_items;
+
+go
+
+SELECT Count(DISTINCT order_id) AS u_order
+FROM   order_items;
+
+go
+
+-- composit key !
+SELECT order_id,
+       order_item_id,
+       Count(*) AS ctn
+FROM   order_items
+GROUP  BY order_id,
+          order_item_id
+HAVING Count(*) > 1;
+
+go
+
+-- check nulls 
+SELECT Sum(CASE
+             WHEN order_id IS NULL THEN 1
+             ELSE 0
+           END) AS order_id_nulls,
+       Sum(CASE
+             WHEN order_item_id IS NULL THEN 1
+             ELSE 0
+           END) AS order_item_id_nulls,
+       Sum(CASE
+             WHEN product_id IS NULL THEN 1
+             ELSE 0
+           END) AS produc_id_nulls,
+       Sum(CASE
+             WHEN seller_id IS NULL THEN 1
+             ELSE 0
+           END) AS seller_id_nulls,
+       Sum(CASE
+             WHEN shipping_limit_date IS NULL THEN 1
+             ELSE 0
+           END) AS shipping_limit_date_nulls,
+       Sum(CASE
+             WHEN price IS NULL THEN 1
+             ELSE 0
+           END) AS price_nulls,
+       Sum(CASE
+             WHEN freight_value IS NULL THEN 1
+             ELSE 0
+           END) AS freight_value_nulls
+FROM   order_items;
+
+go
+
+-- Business Roles 
+go
+
+SELECT Count(*) AS no_price
+FROM   order_items
+WHERE  price <= 0;
+
+go
+
+go
+
+SELECT Count(*) AS zero_freight_val
+FROM   order_items
+WHERE  freight_value < 0;
+
+go
+
+SELECT Count(*) AS free
+FROM   order_items
+WHERE  freight_value = 0;
+
+-- Referential Integrity
+SELECT *
+FROM   order_items oi
+       LEFT JOIN orders o
+              ON oi.order_id = o.order_id
+WHERE  o.order_id IS NULL;
+
+go
+
+SELECT *
+FROM   order_items AS oi
+       LEFT JOIN products p
+              ON oi.product_id = p.product_id
+WHERE  p.product_id IS NULL;
+
+go
+
+SELECT *
+FROM   order_items oi
+       LEFT JOIN sellers s
+              ON oi.seller_id = s.seller_id
+WHERE  s.seller_id IS NULL;
+
+go 
+
+
+---------------------------------------------------------
+---------------------------------------------------------
+
 
