4a04669b43dfd6403f126cc0002a76883bddca38
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-07-19 09:33:36 +0200

    Update 06.EDA.sql

--- 03.SQL/06.EDA.sql
+++ 03.SQL/06.EDA.sql
@@ -29,3 +29,48 @@ FROM   orders;
 
 go 
 
+-- Total Revenue
+SELECT Sum(price) AS total_revenue
+FROM   order_items oi
+       JOIN orders o
+         ON o.order_id = oi.order_id
+WHERE  o.order_status = 'delivered';
+
+go
+
+       ----------------------------------------
+SELECT *
+FROM   products;
+
+go
+
+SELECT *
+FROM   order_items;
+
+go
+
+SELECT Count(*)
+FROM   category_translation
+      -------------------------------------------
+go
+
+-- product categories generate the highest revenue
+SELECT ct.[product_category_name_english] AS Category,
+       Round(Sum(oi.price), 2)             AS Revenue
+FROM   order_items oi
+       JOIN orders o
+         ON o.order_id = oi.order_id
+       JOIN products p
+         ON oi.product_id = p.product_id
+       JOIN category_translation ct
+         ON p.product_category_name = ct.product_category_name
+WHERE  order_status = 'delivered'
+GROUP  BY ct.[product_category_name_english]
+ORDER  BY revenue DESC;
+
+go 
+
+
+
+
+
