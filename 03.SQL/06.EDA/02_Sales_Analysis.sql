daa75ed2c69afa03a584f0a9fe96bc9e1d9a505e
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-07-21 17:48:15 +0200

    Update 02_Sales_Analysis.sql

--- 03.SQL/06.EDA/02_Sales_Analysis.sql
+++ 03.SQL/06.EDA/02_Sales_Analysis.sql
@@ -1,1 +1,30 @@
 
+-- 1. revenue by category
+-------------------------
+
+-- 2. revenue contribution by category
+---------------------------------------
+
+-- 3. monthly revenue trend
+---------------------------
+
+-- 4. top 10 selling categories
+-------------------------------
+
+-- 5. top 10 selling products
+-----------------------------
+
+-- 6. revenue by state
+----------------------
+
+-- 7. revenue by city
+---------------------
+
+-- 8. average product price by category
+---------------------------------------
+
+-- 9. highest priced products
+-----------------------------
+
+-- 10. lowest priced products
+-----------------------------
