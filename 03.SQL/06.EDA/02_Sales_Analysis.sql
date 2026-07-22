c6fe67dfe79b7dc267b3aabc1de3ea64eb678a07
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-07-22 13:18:47 +0200

    Update 02_Sales_Analysis.sql

--- 03.SQL/06.EDA/02_Sales_Analysis.sql
+++ 03.SQL/06.EDA/02_Sales_Analysis.sql
@@ -6,7 +6,20 @@ go
 
 -- 1. revenue by category
 -------------------------
-
+select ct.[ product_category_name_english] as Category  ,
+round(sum(oi.price ),2) as Revenue 
+from order_items oi 
+join orders o 
+on o.order_id = oi.order_id 
+join products p 
+on oi.product_id = p.product_id 
+join category_translation ct 
+on p.product_category_name = ct.product_category_name
+where order_status = 'delivered'
+group by ct.[ product_category_name_english]
+order by Revenue desc; 
+go
+  
 
 -- 2. revenue contribution by category
 ---------------------------------------
