10753a3f0f26bab684f2b77386eda786bc35d90b
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-07-23 21:47:58 +0200

    New vs Returning Customers

--- 03.SQL/06.EDA/04_Customer_Analysis.sql
+++ 03.SQL/06.EDA/04_Customer_Analysis.sql
@@ -59,8 +59,35 @@ group by customer_unique_id
 order by num_of_orders desc; 
 go
 
+
+  
 --4. New vs Returning Customers
 -------------------------------
+go 
+
+select * from orders ;
+go 
+
+select * from customers ; 
+go 
+
+with total_orders as (
+select customer_unique_id , count(order_id) as num_of_orders 
+from orders o 
+join customers c 
+on o.customer_id = c.customer_id 
+where o.order_status = 'delivered'
+group by customer_unique_id 
+) 
+select count(customer_unique_id ) as total_customers , 
+case  when num_of_orders  = 1 then 'New Customer' else 'Returning Customer' end  as customer_status
+from total_orders
+group by case  when num_of_orders  = 1 then 'New Customer' else 'Returning Customer' end  
+
+; 
+go 
+
+
 
 
 
