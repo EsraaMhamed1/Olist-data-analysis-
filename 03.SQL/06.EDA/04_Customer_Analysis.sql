79cd657c7dad37853d8e2f498977cf82f8241666
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-07-23 20:19:10 +0200

    top 10 cities by customers

--- 03.SQL/06.EDA/04_Customer_Analysis.sql
+++ 03.SQL/06.EDA/04_Customer_Analysis.sql
@@ -1,4 +1,5 @@
 --1. Customers by State
+-----------------------
 go 
 
 select * from customers ; 
@@ -10,3 +11,28 @@ group by customer_state
 order by total_customers desc; 
 go 
 
+
+--3. Top 10 Cities by Customers
+-------------------------------
+go 
+
+select * from customers ; 
+go 
+
+select top(10) customer_city , count(distinct customer_unique_id) as total_customers
+from customers 
+group by customer_city 
+order by total_customers desc; 
+go 
+
+--4. New vs Returning Customers
+-------------------------------
+
+--5. Customer Distribution by State
+-----------------------------------
+
+--6. Customers per Order
+------------------------
+
+--7. Average Orders per Customer
+--------------------------------
